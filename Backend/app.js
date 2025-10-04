1ddfd3a421b2684fad8b6e12d1893bbc133bda2d
lYNUX <lynux@gmail.com>
2025-10-04 11:02:39 +0530

    root endpoint hit

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
 const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-
 dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('Expense Management API is running');
+    console.log('Root endpoint hit');
 });
+
+
+
+module.exports = app;
